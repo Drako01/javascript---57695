21dc422ce164cf66e4ea246d10cebd41ce5ed156
Alejandro Di Stefano <addistefano@live.com.ar>
2024-06-06 08:22:42 -0300

    Clase 15

---
+++ js/navegacion.js
@@ -0,0 +1,32 @@
+// Inicializo la pagina
+const cabeza = document.head;
+const cuerpo = document.body;
+const main = document.getElementsByTagName('main')[0];
+const titulo = document.createElement('title');
+titulo.innerHTML = 'Comisión #57695 | CoderHouse';
+cabeza.appendChild(titulo); // Agrego el titulo de la Pagina
+
+const h1Titulo = document.createElement('h1');
+h1Titulo.innerHTML = titulo.innerHTML;
+
+h1Titulo.style.color = 'blue';
+h1Titulo.classList.add('titulo');
+
+const cabecera = document.createElement('header');
+const navegacion = document.createElement('navbar');
+const nav = document.createElement('nav');
+const ul = document.createElement('ul');
+cuerpo.prepend(cabecera);
+main.appendChild(h1Titulo);
+cabecera.appendChild(navegacion);
+navegacion.className = 'navbar';
+navegacion.appendChild(nav);
+nav.appendChild(ul);
+
+const links = ["Index", "Products", "Contact"];
+
+for (const link of links) {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="${link.toLowerCase()}.html" >${link}</a>`
+    ul.appendChild(li);
+}
